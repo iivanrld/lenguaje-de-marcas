0a44e0ee147b2211554ad5c3a5b6fbb00488385f
iivanrld <151529210+iivanrld@users.noreply.github.com>
2024-02-01 11:08:54 +0100

    Add files via upload

---
+++ ejercicios_javascript/01-consol-log/02-variables/script.js
@@ -0,0 +1,8 @@
+let nombre = "Juan";
+let apellidos = "Pérez";
+let animal = "Perro";
+
+console.log("mi primer ejercicio de JS");
+console.log("Nombre: " + nombre);
+console.log("Apellidos: " + apellidos);
+console.log("Animal preferido: " + animal);
